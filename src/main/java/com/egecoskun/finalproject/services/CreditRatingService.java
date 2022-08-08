16a224c7e603fa5dd9ab44775a471d0dcc2bf3b8
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-08-08 22:43:06 +0300

    Added Credit and CreditRating Service

--- src/main/java/com/egecoskun/finalproject/services/CreditRatingService.java
+++ src/main/java/com/egecoskun/finalproject/services/CreditRatingService.java
@@ -1,7 +1,64 @@
 package com.egecoskun.finalproject.services;
 
+import com.egecoskun.finalproject.exception.EntityNotFoundException;
+import com.egecoskun.finalproject.model.Credit;
+import com.egecoskun.finalproject.model.CreditRating;
+import com.egecoskun.finalproject.model.DTO.CreditRatingDTO;
+import com.egecoskun.finalproject.model.mapper.CreditRatingMapper;
+import com.egecoskun.finalproject.repository.CreditRatingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Random;
+
 @Service
+@Slf4j
 public class CreditRatingService {
+
+
+    private final int LOWER_BOUND=0;
+    private final int UPPER_BOUND=1900;
+    private final CreditRatingRepository creditRatingRepository;
+    private final ApplicantService applicantService;
+
+    @Autowired
+    public CreditRatingService(CreditRatingRepository creditRatingRepository, ApplicantService applicantService) {
+        this.creditRatingRepository = creditRatingRepository;
+        this.applicantService = applicantService;
+    }
+
+    public CreditRating getById(Long id) {
+        Optional<CreditRating> byId = creditRatingRepository.findById(id);
+        return byId.orElseThrow(() -> {
+            log.error("Credit rating not found by id : " + id);
+            return new EntityNotFoundException("Credit rating ", "id : " + id);
+        });
+    }
+
+    public CreditRating getByApplicantId(Long id) {
+        Optional<CreditRating> byId = Optional.ofNullable(applicantService.getById(id).getCreditRating());
+        return byId.orElseThrow(() -> {
+            log.error("Credit rating not found by applicant id : " + id);
+            return new EntityNotFoundException("Credit rating", "applicant id : " + id);
+        });
+    }
+
+    public int getRandomCreditRating(){
+        Random random = new Random();
+        int randomCreditRating = random.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+        return randomCreditRating;
+    }
+
+    public CreditRating create(CreditRatingDTO creditRatingDTO) {
+        CreditRating creditRating = CreditRatingMapper.toEntity(creditRatingDTO);
+        creditRating.setCreditRating(getRandomCreditRating());
+        return creditRatingRepository.save(creditRating);
+    }
+
+    public void delete(Long id) {
+        getById(id);
+        creditRatingRepository.deleteById(id);
+    }
 }
--- src/main/java/com/egecoskun/finalproject/services/CreditService.java
+++ src/main/java/com/egecoskun/finalproject/services/CreditService.java
@@ -3,9 +3,8 @@ package com.egecoskun.finalproject.services;
 import com.egecoskun.finalproject.exception.EntityNotFoundException;
 import com.egecoskun.finalproject.model.Applicant;
 import com.egecoskun.finalproject.model.Credit;
-import com.egecoskun.finalproject.model.DTO.ApplicantDTO;
 import com.egecoskun.finalproject.model.DTO.CreditDTO;
-import com.egecoskun.finalproject.model.mapper.ApplicantMapper;
+
 import com.egecoskun.finalproject.model.mapper.CreditMapper;
 import com.egecoskun.finalproject.repository.CreditRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -13,18 +12,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 @Slf4j
 @Service
 public class CreditService {
+
+    TreeMap<Integer, Integer> gradeMap = (TreeMap<Integer, Integer>) Map.of(0,0,
+                                                                            500,1,
+                                                                            1000,2);
+
+
     
     private final CreditRepository creditRepository;
+    private final ApplicantService applicantService;
 
     @Autowired
-    public CreditService(CreditRepository creditRepository) {
+    public CreditService(CreditRepository creditRepository, ApplicantService applicantService) {
         this.creditRepository = creditRepository;
+        this.applicantService = applicantService;
     }
+
     
     public List<Credit> getAllCredits() {
         List<Credit> allCredits = creditRepository.findAll();
@@ -48,4 +58,14 @@ public class CreditService {
         getById(id);
         creditRepository.deleteById(id);
     }
+
+    public Credit getCreditByIdentificationNumber(Long identificationNumber){
+        Optional<Applicant> applicant = applicantService.getByIdentificationNumber(identificationNumber);
+        Credit credit = applicant.get().getCredit();
+        return credit;
+    }
+
+
+
+
 }
