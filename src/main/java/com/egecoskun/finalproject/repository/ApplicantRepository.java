98f1ff80d51560946398b1c9c195a00daa19245b
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-08-08 22:44:13 +0300

    Added custom sql Query getApplicantByIdentificationNumber

--- src/main/java/com/egecoskun/finalproject/repository/ApplicantRepository.java
+++ src/main/java/com/egecoskun/finalproject/repository/ApplicantRepository.java
@@ -2,6 +2,14 @@ package com.egecoskun.finalproject.repository;
 
 import com.egecoskun.finalproject.model.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ApplicantRepository extends JpaRepository<Applicant,Long> {
+
+    @Query(value = "SELECT * FROM applicant WHERE applicant.identification_number=:identificationNumber",nativeQuery = true)
+    Optional<Applicant> getApplicantByIdentificationNumber(@Param("identificationNumber") Long identificationNumber);
+
 }
