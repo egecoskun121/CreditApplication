0cc7301154648defd94311a224d86087f106083d
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-08-08 22:41:52 +0300

    refactor

--- src/main/java/com/egecoskun/finalproject/model/DTO/ApplicantDTO.java
+++ src/main/java/com/egecoskun/finalproject/model/DTO/ApplicantDTO.java
@@ -2,12 +2,11 @@ package com.egecoskun.finalproject.model.DTO;
 
 import lombok.Data;
 
-import java.math.BigInteger;
 
 @Data
 public class ApplicantDTO {
 
-    private BigInteger identificationNumber;
+    private Long identificationNumber;
     private String firstName;
     private String lastName;
     private double monthlyIncome;
