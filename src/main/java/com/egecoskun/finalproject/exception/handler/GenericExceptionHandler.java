8228a97dbe1b76a2a62e60a29fe166e48e3f5e38
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-08-10 17:45:35 +0300

    refactor GenericExceptionHandler

--- src/main/java/com/egecoskun/finalproject/exception/handler/GenericExceptionHandler.java
+++ src/main/java/com/egecoskun/finalproject/exception/handler/GenericExceptionHandler.java
@@ -2,8 +2,11 @@ package com.egecoskun.finalproject.exception.handler;
 
 
 import com.egecoskun.finalproject.exception.ApplicantNotFoundException;
+import com.egecoskun.finalproject.exception.CustomJwtException;
+import com.egecoskun.finalproject.exception.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,7 +16,6 @@ import java.util.Map;
 @ControllerAdvice
 public class GenericExceptionHandler {
 
-
     @ExceptionHandler(ApplicantNotFoundException.class)
     public ResponseEntity<Map<String,String>> handleApplicantNotFoundException(ApplicantNotFoundException e){
         Map<String,String> errorResponseMap = new HashMap<>();
@@ -21,5 +23,35 @@ public class GenericExceptionHandler {
         errorResponseMap.put("error_cause",e.getCause().toString());
 
         return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseMap);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleEntityNotFoundException(EntityNotFoundException exception) {
+        Map<String, String> errorResponseMap = new HashMap<>();
+        errorResponseMap.put("error_message", exception.getMessage());
+        errorResponseMap.put("error_details", exception.getDetails());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseMap);
+    }
+
+    @ExceptionHandler(CustomJwtException.class)
+    public ResponseEntity<Map<String, String>> handleCustomJwtException(CustomJwtException exception) {
+        Map<String, String> errorResponseMap = new HashMap<>();
+        errorResponseMap.put("error_message", exception.getMessage());
+        errorResponseMap.put("error_status", exception.getHttpStatus().toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseMap);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDeniedException(AccessDeniedException exception) {
+        Map<String, String> errorResponseMap = new HashMap<>();
+        errorResponseMap.put("error_message", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponseMap);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleEntityNotFoundException(Exception exception) {
+        Map<String, String> errorResponseMap = new HashMap<>();
+        errorResponseMap.put("error_message", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseMap);
     }
 }
