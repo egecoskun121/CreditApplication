ec973862c32b4385fbf4fcc9ca29ce43f819120b
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-08-13 13:59:40 +0300

    Added CreditException

---
+++ src/main/java/com/egecoskun/finalproject/exception/CreditException.java
@@ -0,0 +1,7 @@
+package com.egecoskun.finalproject.exception;
+
+public class CreditException extends RuntimeException{
+   public CreditException(String message){
+        super(message);
+    }
+}
